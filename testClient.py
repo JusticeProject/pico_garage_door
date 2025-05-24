9ac4a382a002ad9f37f559a60c1a7ec4ff0f7be8
JusticeProject <93150828+JusticeProject@users.noreply.github.com>
2025-05-24 16:25:36 -0500

    client test script updated with encryption

--- testClient.py
+++ testClient.py
@@ -1,4 +1,37 @@
 import socket
+from Crypto.Cipher import AES
+
+###############################################################################
+
+# a sample shared key
+preshared_key = b"\x07\x8b\xf07\x1b\x94\xbfz=\xfa&D\xa2\x07\x0e\x00\x81~\x83\x96\xe5.j\x19'\x9f\r?\xa9\xbb\x00h"
+
+###############################################################################
+
+def decryptFromPicoW(cipherText):
+    decrypter = AES.new(preshared_key, AES.MODE_ECB)
+    plainText = decrypter.decrypt(cipherText)
+    return plainText
+
+###############################################################################
+
+def manipulateBytes(data):
+    newData = []
+    for i in range(0, len(data)):
+        newByte = data[i] + i + 1
+        if (newByte > 255):
+            newByte -= 255
+        newData.append(newByte)
+    return bytes(newData)
+
+###############################################################################
+
+def encryptToPicoW(plainText):
+    encrypter = AES.new(preshared_key, AES.MODE_ECB)
+    cipherText = encrypter.encrypt(plainText)
+    return cipherText
+
+###############################################################################
 
 server_addr_port = socket.getaddrinfo("picow.local", 12345)[0][-1]
 print(server_addr_port)
@@ -10,5 +43,8 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.sendto(b"Knock", server_addr_port)
 
 # Receive data from the server
-data, addr = client_socket.recvfrom(1024)
-print(f"Received {len(data)} bytes")
+cipherText, addr = client_socket.recvfrom(1024)
+plainText = decryptFromPicoW(cipherText)
+newPlainText = manipulateBytes(plainText)
+newCipherText = encryptToPicoW(newPlainText)
+client_socket.sendto(newCipherText, server_addr_port)
