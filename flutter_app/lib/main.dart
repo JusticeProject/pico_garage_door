fef22e6f976834a6b9d1713144b71de4eecb70b9
JusticeProject <93150828+JusticeProject@users.noreply.github.com>
2025-05-25 16:00:28 -0500

    adding flutter app, work in progress

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'utilities.dart' as utilities;
+
+//*************************************************************************************************
+
+void main() {
+  runApp(const MyApp());
+}
+
+//*************************************************************************************************
+
+class MyApp extends StatelessWidget
+{
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      title: 'Garage Door Opener',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+      ),
+      home: const MyHomePage(title: 'Pico Garage Door Opener'),
+    );
+  }
+}
+
+//*************************************************************************************************
+
+class MyHomePage extends StatefulWidget
+{
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+//*************************************************************************************************
+
+class _MyHomePageState extends State<MyHomePage>
+{
+  final _textController = TextEditingController();
+
+  @override
+  void dispose()
+  {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _onOpenPressed()
+  {
+
+  }
+
+  void _onScanPressed() async
+  {
+    print("Before scan, text field = ${_textController.text}");
+
+    String addr = await utilities.lookupHostname("raspberrypi.local");
+    if (addr.isEmpty)
+    {
+      addr = "?";
+    }
+
+    print("Found addr = $addr");
+    // TODO: do I need to put setState() around this? It seems to work without it.
+    // This call to setState tells the Flutter framework that something has
+    // changed in this State, which causes it to rerun the build method below
+    // so that the display can reflect the updated values. If we changed
+    // the variable without calling setState(), then the build method would not be
+    // called again, and so nothing would appear to happen.
+    setState(() {
+      _textController.text = addr;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(onPressed: _onOpenPressed, child: Text("Open / Close Door")),
+            SizedBox(height: 20),
+            SizedBox(
+              width: 200,
+              child: TextField(
+                controller: _textController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "IP Address",
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            //Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
+            ElevatedButton(onPressed: _onScanPressed, child: Text("Scan for PicoW"))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_app/lib/utilities.dart
@@ -0,0 +1,147 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:pointycastle/export.dart';
+// see https://pub.dev/packages/pointycastle
+
+//*************************************************************************************************
+
+const keyBytes = [7,139,240,55,27,148,191,122,61,250,38,68,162,7,14,0,129,126,131,150,229,46,106,25,39,159,13,63,169,187,0,104,];
+
+//*************************************************************************************************
+
+/*void main() async
+{
+  Uint8List presharedKey = Uint8List.fromList(keyBytes);
+  print("key length = ${presharedKey.length}");
+  print(presharedKey);
+
+  String addr = "";
+  for (int i = 0; i < 10; i++)
+  {
+    addr = await lookupHostname("picow.local");
+    if (addr.isNotEmpty)
+    {
+      break;
+    }
+  }
+
+  // if it's still empty, use the default address
+  if (addr.isEmpty)
+  {
+    print("using default address");
+    addr = "192.168.1.160";
+  }
+
+  print("found address for PicoW: $addr");
+
+  await sendCmd(addr, presharedKey);
+}*/
+
+//*************************************************************************************************
+
+Future<String> lookupHostname(String hostname) async
+{
+  try
+  {
+    List<InternetAddress> addrs = await InternetAddress.lookup(hostname, type: InternetAddressType.IPv4);
+    return addrs.first.address;
+  }
+  on Exception catch (e)
+  {
+    print(e);
+    return "";
+  }
+}
+
+//*************************************************************************************************
+
+// from https://api.dart.dev/dart-io/RawDatagramSocket-class.html
+Future<void> sendCmd(String addr, Uint8List key) async
+{
+  final clientSocket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
+  
+  List<int> asciiValues = "Knock".codeUnits;
+  Uint8List knockPacket = Uint8List.fromList(asciiValues);
+  print(knockPacket);
+
+  int bytesWritten = clientSocket.send(knockPacket, InternetAddress(addr), 12345);
+  print("wrote $bytesWritten bytes");
+
+  clientSocket.listen((event)
+  {
+    switch (event)
+    {
+      case RawSocketEvent.read:
+        final datagram = clientSocket.receive();
+        print("read event:");
+        print("${datagram!.data.length} bytes");
+        Uint8List cipherText = datagram.data;
+        Uint8List plainText = decryptFromPicoW(key, cipherText);
+        Uint8List newPlainText = manipulateBytes(plainText);
+        Uint8List newCipherText = encryptToPicoW(key, newPlainText);
+        int bytesWritten = clientSocket.send(newCipherText, InternetAddress(addr), 12345);
+        print("wrote $bytesWritten bytes");
+        clientSocket.close();
+        break;
+      case RawSocketEvent.write:
+        print("write event");
+        break;
+      case RawSocketEvent.closed:
+        print("closed event");
+        break;
+      default:
+        print("unexpected event $event");
+    }
+  });
+}
+
+//*************************************************************************************************
+
+Uint8List decryptFromPicoW(Uint8List key, Uint8List cipherText)
+{
+  var decrypter = ECBBlockCipher(AESEngine());
+  decrypter.init(false, KeyParameter(key));
+  Uint8List plainText = Uint8List(cipherText.length); // allocate space
+
+  var offset = 0;
+  while (offset < cipherText.length)
+  {
+    offset += decrypter.processBlock(cipherText, offset, plainText, offset);
+  }
+
+  return plainText;
+}
+
+//*************************************************************************************************
+
+Uint8List manipulateBytes(Uint8List input)
+{
+  //Uint8List output = Uint8List(input.length);
+  for (int i = 0; i < input.length; i++)
+  {
+    int number = input[i] + i + 1;
+    if (number > 255)
+    {
+      number -= 255;
+    }
+    input[i] = number;
+  }
+  return input;
+}
+
+//*************************************************************************************************
+
+Uint8List encryptToPicoW(Uint8List key, Uint8List plainText)
+{
+  var encrypter = ECBBlockCipher(AESEngine());
+  encrypter.init(true, KeyParameter(key));
+  Uint8List cipherText = Uint8List(plainText.length); // allocate space
+
+  var offset = 0;
+  while (offset < plainText.length)
+  {
+    offset += encrypter.processBlock(plainText, offset, cipherText, offset);
+  }
+
+  return cipherText;
+}
