527f834ff47138279728c4d2c607b6fb2e874767
JusticeProject <93150828+JusticeProject@users.noreply.github.com>
2025-05-27 10:41:46 -0500

    adding optocoupler, led flashing

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import ucryptolib
 
 ###############################################################################
 
+OPTOCOUPLER_GPIO_PIN = 0
+
 # This is just one example AES key. To generate a new one that is not committed to GitHub repo then run generateKey.py
 preshared_key = b"\x07\x8b\xf07\x1b\x94\xbfz=\xfa&D\xa2\x07\x0e\x00\x81~\x83\x96\xe5.j\x19'\x9f\r?\xa9\xbb\x00h"
 
@@ -57,38 +59,45 @@ def verifyChallenge(challenge, response):
         return True
     else:
         return False
+    
+###############################################################################
+
+def flashLED(led : Pin, count : int):
+    for i in range(0, count):
+        led.on()
+        time.sleep_ms(250)
+        led.off()
+        time.sleep_ms(250)
 
 ###############################################################################
 
-# status LED
+# status LED that's on the PicoW board
 led = Pin("LED", Pin.OUT)
 led.on()
+
+# optocoupler - on will trigger the garage door
+opto = Pin(OPTOCOUPLER_GPIO_PIN, Pin.OUT)
+opto.off()
 
 ###############################################################################
 
 network.country("US") # or import rp2  then rp2.country("US")
 network.hostname("picow")
 wlan = network.WLAN(network.STA_IF)
-print("wlan created")
 wlan.active(True)
-print("wlan active")
 wlan.connect(secrets.SSID, secrets.PASSWORD)
-print("connecting")
  
 # Wait for connection
 while not wlan.isconnected():
-    time.sleep(1)
-    print("waiting...")
-print('WiFi connected')
-print(wlan.ifconfig())
+    time.sleep(1)   
+#print(wlan.ifconfig())
 
 ###############################################################################
 
 # Set up UDP server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((wlan.ifconfig()[0], 17812))
-print("Server is Up and Listening")
-print(wlan.ifconfig()[0])
+#print(wlan.ifconfig()[0])
 
 led.off()
 random.seed(time.time())
@@ -100,14 +109,14 @@ challengeSentTime = 0
 
 try:
     while True:
-        print('Waiting for a request from the client...')
+        #print('Waiting for a request from the client...')
         data, client_address = server_socket.recvfrom(1024)
-        print(len(data))
+        #print(len(data))
 
         # check if we timed out
         now = time.time()
         if (len(newChallenge) > 0) and (now - challengeSentTime > 20):
-            print("Challenge timed out")
+            #print("Challenge timed out")
             newChallenge = b""
         
         # if len is 10 then it's probably a request for a challenge, else check if it's the right block size for our AES data
@@ -116,19 +125,18 @@ try:
             ciphertext = encryptChallenge(newChallenge)
             server_socket.sendto(ciphertext, client_address)
             challengeSentTime = time.time()
-            led.off()
         elif (len(data) == 128):
             verified = verifyChallenge(newChallenge, data)
             if verified:
-                led.on()
+                opto.on()
+                time.sleep_ms(250)
+                opto.off()
+                flashLED(led, 1)
                 newChallenge = b""
             else:
-                print("Challenge failed")
-                led.off()
+                flashLED(led, 2)
         else:
-            print("Unknown data")
-            led.off()
+            flashLED(led, 3)
 
 except KeyboardInterrupt:
-    print("interrupted")
     wlan.disconnect()
